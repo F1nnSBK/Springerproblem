2390794a1f28f54001a087bf3f632abdf0caaf42
finnhertsch <fhertsch235@gmail.com>
2024-10-04 07:49:09 +0200

    Schachbrett erzeugt

--- src/Schachbrett.java
+++ src/Schachbrett.java
@@ -5,44 +5,60 @@ import java.awt.event.WindowEvent;
 import javax.swing.*;
 
 public class Schachbrett {
-    private JFrame mainFrame;
 
+    //private --> nur in dieser Klasse verfügbar
+
+    // Kontruktor Schachbrett() --> ruft Methode launchGUI() auf
     public Schachbrett() {
         launchGUI();
     }
 
+    // main-Methode --> Schachbrett() Konstruktor --> launchGUI() Methode
     public static void main(String[] args) {
         Schachbrett swingSchachbrett = new Schachbrett();
     }
 
+    // private Methode ohne Rückgabewert (void) öffnet die GUI
     private void launchGUI() {
+        // mainFrame Instanzvariable JFrame (JFrame-Objekt) --> Hauptfenster
+        JFrame mainFrame;
+        // Erstellt neue Instanz von JFrame mit dem Titel Schachbrett
         mainFrame = new JFrame("Schachbrett");
+        // Größe dieses Fensters
         mainFrame.setSize(800, 600);
+        // Setzt das Layout auf "Gridlayout" (AWT Layout Klasse)
         mainFrame.setLayout(new GridLayout(8, 8));
 
         // Schachmuster erzeugen
-
+        /*  iteriert über jedes der 64 Felder und prüft, ob das Feld weiß oder nicht weiß ist.
+            Ist white wahr, wird das Feld weiß und der Wert der boolean Variable white wird umgekehrt.
+            Das nächste Feld ist dann !white und bekommt durch den else Fall die Farbe Weiß.  */
         boolean white = true;
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
+                // Erzeugt die Instanzvariable square des Typs JPanel (JPanel-Objekt)
                 JPanel square = new JPanel();
                 if (white) {
-                    square.setBackground(Color.DARK_GRAY);
-                }
-                else {
                     square.setBackground(Color.WHITE);
                 }
+                else {
+                    square.setBackground(Color.DARK_GRAY);
+                }
+                // Das square wird dem Hauptfenster zugeteilt, damit es dargestellt werden kann.
                 mainFrame.add(square);
                 white = !white;
             }
             white = !white;
         }
 
+        // Beendet die Anwendung (System.exit()) wenn das Fenster geschlossen wird
         mainFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent) {
                 System.exit(0);
             }
         });
+
+        // Verhindert das automatische Schließen des Fensters
         mainFrame.setVisible(true);
     }
 
