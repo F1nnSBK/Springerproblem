8fce8b50a2ec22dad1350027ec42191d04537137
finnhertsch <fhertsch235@gmail.com>
2024-10-07 10:15:29 +0200

    Finish + Kommentierung von Springer.java

--- src/schachbrett/Springer.java
+++ src/schachbrett/Springer.java
@@ -1,12 +1,28 @@
 package schachbrett;
 
+/**
+ * Dieses Programm enthält die Logik hinter dem Springerproblem (Knight's tour) von Euler
+ * Außerdem wird ein Array mit der Reihenfolge der Sprünge des Springers an die Schachbrettklasse
+ * weitergegeben.
+ * Als Ausgabe gibt es eine Tabelle, welche die Reihenfolge der Züge anzeigt.
+ *
+ * @author Finn Hertsch
+ * @version 1.0.0
+ */
+
 public class Springer {
     // Erstellt eine Instanzvariable der Schachbrettgröße N aus Schachbrett.java
     static int N = Schachbrett.N;
     // Erstellt das 2D path Array basierend auf der Schachbrettgröße
     private static final int[][] path = new int[Schachbrett.N*Schachbrett.N][2];
 
-    // Konstruktor Springer() --> ruft Methode springerBewegungen() auf
+    /**
+     * Dient als Konstruktor
+     * Initialisiert das board Array, welches das Schachbrett widerspiegelt,
+     * Gibt die Größe des Schachfeldes aus und setzt alle Felder des Schachfeldes auf den Wert -1,
+     * was bedeutet, dass alle Felder unbesucht sind. Abschließend wird die Funktion zur Berechnung
+     * der Bewegungen des Springers aufgerufen.
+     */
     public Springer() {
         // 2D Array als Schachbrett
         int[][] board = new int[N][N];
@@ -22,16 +38,33 @@ public class Springer {
         springerBewegungen(board);
     }
 
-    // Wird in Schachbrett.java aufgerufen, um das path objekt zu bekommen
+    /**
+     * Diese Methode getPath() gibt das path array weiter, wenn sie aufgerufen wird.
+     *
+     * @return path als 2D Array, enthält die Züge des Springers
+     */
     public int[][] getPath() {
         return path;
     }
 
-    // main-Methode --> Springer() Konstruktor --> springerBewegungen() Methode
+    /**
+     * Main Methode, beschreibt die Commandline Befehle
+     * Ruft die Methode Springer() auf.
+     *
+     * @param args zeigt, dass es sich in der Methode um die Commandline Argumente handelt, welche beim
+     *             Starten des Programms ausgeführt werden.
+     */
     public static void main(String[] args) {
         new Springer();
     }
 
+    /**
+     * Diese Methode dient als Driver-Methode (oder Wrapper-Methode), die die Springerbewegungen auf dem Schachbrett steuert.
+     * Sie initialisiert die Startposition auf dem Schachbrett und die möglichen Züge des Springers.
+     * Ruft die rekursive Methode springerBewegungUtil auf, um die Springerbewegungen auszuführen.
+     *
+     * @param board das 2D-Array, das das Schachbrett darstellt
+     */
     private void springerBewegungen(int[][] board) {
         // Alle möglichen Züge des Springers → immer als Paar zu betrachten (x, y) 'L' Form
         int[] xMoves = {1,2,2,1,-1,-2,-2,-1};
@@ -54,6 +87,12 @@ public class Springer {
         }
     }
 
+    /**
+     * Diese Methode dient zur Ausgabe der Lösung im Format einer Tabelle
+     * Gibt die Schritte des Springers mit korrekten Abständen in das Terminal aus.
+     *
+     * @param arr steht, für das board array und enthält den moveCount (Zugnummer)
+     */
     static void springerLoesung(int[][] arr) {
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
@@ -71,12 +110,37 @@ public class Springer {
         }
     }
 
-    // Prüft, ob der berechnete Schritt im Bereich des Schachbretts ist und ob dieses Feld unbesucht (-1) als Wert hat
+    /**
+     * Diese Methode Prüft, ob der jeweilige Schritt des Springers gültig ist.
+     * Ist der Zug innerhalb des Schachfeldes?
+     * Ist das Feld unbesucht (Wert -1)?
+     *
+     * @param board Array, welches das Schachfeld darstellt
+     * @param row Reihen auf dem Schachfeld
+     * @param col Spalten auf dem Schachfeld
+     * @return Gibt einen boolean Wert aus true/false, ob der Zug gültig ist oder nicht
+     */
     static boolean isValid(int[][] board, int row, int col) {
         return(row >= 0 && row < N && col >= 0 && col < N && board[row][col] == -1);
     }
 
-
+    /**
+     * Diese Hilfsmethode (oder Utility-Methode) oder spez. Backtracking-Methode
+     * berechnet den nächsten Schritt des Springers und geht einen Schritt zurück, wenn der
+     * Schritt nicht gültig ist.
+     * Dafür nutzt sie die Liste aller möglichen Bewegungen aus der Driver-Methode und
+     * ruft sich selbst auf, um den nächsten Zug zu finden.
+     *
+     * @param board Array, welches das Schachbrett darstellt
+     * @param xMoves Array, mit allen möglichen Bewegungen in x-Richtung
+     * @param yMoves Array, mit allen möglichen Bewegungen in y-Richtung
+     * @param row aktuelle Zeile
+     * @param col aktuelle Spalte
+     * @param moveCount aktuelle Nummer des Zuges
+     * @see Springer().isValid() gibt an, ob der Zug gültig ist oder nicht
+     * @return true, wenn die Anzahl aller Schritte erreicht ist und jedes Feld besucht wurde
+     *         und false, wenn es keine weitere Lösung mehr gibt
+     */
     static boolean springerBewegungUtil(int[][] board, int[] xMoves, int[] yMoves, int row, int col, int moveCount) {
         // Wenn die Anzahl der moves 64 erreicht hat, wird true weitergegeben, was das Programm stoppt
         if(moveCount == N*N) {
